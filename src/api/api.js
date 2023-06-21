cda9091ad44418877f7d761f3fe7870a9a1a6fa0
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-06-21 17:33:46 +0300

    r

--- src/api/api.js
+++ src/api/api.js
@@ -4,13 +4,11 @@ axios.defaults.baseURL = 'https://649012571e6aa71680ca7dc5.mockapi.io';
 
 export const fetchContacts = async () => {
   const { data } = await axios.get('/contacts');
-  console.log(data);
   return data;
 };
 
 export const addContact = async ({ name, number }) => {
   const { data } = await axios.post('/contacts', { name, number });
-  console.log(data);
   return data;
 };
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,18 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import css from './Phonebook.module.css';
 import { fetchContacts } from 'api/api';
-import { selectError, selectIsLoading } from 'redux/selectors';
+// import { selectError, selectIsLoading } from 'redux/selectors';
 
 export const App = () => {
   fetchContacts();
 
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
 
   return (
     <div className={css.container}>
       <h1 className={css.title}>Phonebook</h1>
-      {isLoading && !error && <p>Request in progress...</p>}
+      {/* {isLoading && !error && <p>Request in progress...</p>} */}
       <AddContactForm />
       <h2 className={css.titleFilter}>Contacts</h2>
       <Filter />
